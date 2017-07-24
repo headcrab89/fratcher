128792768b278e35bbbdbd40a64b4d961deb8e62
Olga Groh <o_groh@student.uni-kassel.de>
2017-07-24 18:38:43 +0200

    Return correct server url

--- src/main/java/com/webengineering/fratcher/util/AddressService.java
+++ src/main/java/com/webengineering/fratcher/util/AddressService.java
@@ -1,61 +1,19 @@
 package com.webengineering.fratcher.util;
 
-import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.net.InetAddress;
-
 @Service
-@Configuration
-public class AddressService implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
-    private int port;
+public class AddressService  {
+    @Value("${addressService.address}")
+    private String serverAddress;
 
     /**
-     * Return the host address as an IP address.
-     *
-     * @return address
-     */
-    public String getHostAddress() {
-        return InetAddress.getLoopbackAddress().getHostAddress();
-    }
-
-    /**
-     * Return the host address as a DNS-resolvable name.
-     *
-     * @return address
-     */
-    public String getHostName() {
-        return InetAddress.getLoopbackAddress().getHostName();
-    }
-
-    /**
-     * This method is called when a particular event (noted in the interface) is executed. In our case, the event
-     * was the start of the embedded application container as statet in the generic parameter.
-     *
-     * @param event the occurred event
-     */
-    @Override
-    public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
-        port = event.getEmbeddedServletContainer().getPort();
-    }
-
-    /**
-     * Return the port of the application.
-     *
-     * @return port
-     */
-    public int getPort() {
-        return port;
-    }
-
-    /**
-     * Return server URL with http:// prefix.
+     * Return server URL.
      *
      * @return server URL.
      */
     public String getServerURL() {
-        return "http://" + getHostName() + ":" + getPort() + "/";
+        return serverAddress;
     }
 }
