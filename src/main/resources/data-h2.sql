67b947317dfd150d8395cc881f2a75ff28643590
Olga Groh <o_groh@student.uni-kassel.de>
2017-08-07 20:42:44 +0200

    Add and change the users in database

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -7,16 +7,17 @@ DELETE FROM USER_;
 
   -- Insert new users.
   INSERT INTO USER_ (id, email, PASSWORD) VALUES
-  (1, 'blub', 'b80e09ccf074b8cd1c19933071f11925a00765cf091b46d5877eb69a05689c90faf6ee92611528b05c0aaa8949754f7cd51b4911822195e242c9e413256feed4'), --foo
-  (2, 'test', '2b97e61878fa6d1221e80d13f719892128c2806939ff5efae428198b3512efb7f631f0e47fac63a96dd0569e75f9574ca3a72116197cc57771f7d4ad2dd76607'), --bar
-  (3, 'initUser', 'e47c99116679030a474594c215ca7512c2a2a4fd3cf29b72fdb24e6d7ae491bc49854497b9268e5b1b29d1b4c8179d6fba2b8a3842415056b38d8411fde7daab'); --test;
-
+  (1, 'Max', 'b80e09ccf074b8cd1c19933071f11925a00765cf091b46d5877eb69a05689c90faf6ee92611528b05c0aaa8949754f7cd51b4911822195e242c9e413256feed4'), --foo
+  (2, 'Anna', '2b97e61878fa6d1221e80d13f719892128c2806939ff5efae428198b3512efb7f631f0e47fac63a96dd0569e75f9574ca3a72116197cc57771f7d4ad2dd76607'), --bar
+  (3, 'Stefan', '4dd8c1a950d6ce914dc8fab783324bb506d1355d5d6d2cca4521e4aaa9a1c7a3c5499fed215bd98e98418d3e623b22b0f01bad0d11eedc4f111858d2f1d9aee5'), --test;
+  (4, 'Alice', 'b80e09ccf074b8cd1c19933071f11925a00765cf091b46d5877eb69a05689c90faf6ee92611528b05c0aaa8949754f7cd51b4911822195e242c9e413256feed4'); --foo
 
   -- Add some posts.
   INSERT INTO TEXT (ID, USER_TEXT, CREATED_AT, AUTHOR_ID) VALUES
-  (1, 'Hallo', parsedatetime('2017-07-20 05:01', 'yyyy-MM-dd HH:mm'), 1),
-  (2, 'Huhu', parsedatetime('2017-07-21 12:01', 'yyyy-MM-dd HH:mm'), 2),
-  (3, 'Hi', parsedatetime('2017-07-23 12:01', 'yyyy-MM-dd HH:mm'), 3);
+  (1, 'Dies ist nicht Vietnam, sondern Bowling, da gibt es Regeln.', parsedatetime('2017-07-20 05:01', 'yyyy-MM-dd HH:mm'), 1),
+  (2, 'Ich weiß, dass ich nichts weiß.', parsedatetime('2017-07-21 12:01', 'yyyy-MM-dd HH:mm'), 2),
+  (3, 'Die Frage: nach dem Leben, dem Universum und dem ganzen Rest. - 42.', parsedatetime('2017-07-23 12:01', 'yyyy-MM-dd HH:mm'), 3),
+  (4, 'Bis zur Unendlichkeit und noch viel weiter!',  parsedatetime('2017-08-07 12:01', 'yyyy-MM-dd HH:mm'), 4);
 
   -- Add some matches
   INSERT INTO MATCH_ (ID, INIT_USER_ID, MATCH_USER_ID, MATCH_STATUS) VALUES
