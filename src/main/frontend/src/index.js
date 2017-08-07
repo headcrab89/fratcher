4d32c7855055de0febf7a2c1ac45f2e754805790
Olga Groh <o_groh@student.uni-kassel.de>
2017-08-07 21:56:56 +0200

    Add rounting with a simple header

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {CookiesProvider} from "react-cookie";
 import ReactDOM from "react-dom";
+import {HashRouter as Router, Link, Route, Switch} from "react-router-dom";
 
 import Authentication from "./components/authentication";
 import TextList from "./components/text_list";
@@ -8,11 +9,22 @@ import MatchList from "./components/match_list";
 
 ReactDOM.render(
     <CookiesProvider>
-        <div>
-            <TextList />
-            <MatchList />
-            <Authentication/>
-        </div>
+        <Router>
+            <div>
+                <div className="menu">
+                    <Link to="/match/find">find match</Link>
+                    <Link to="/match/list">list match</Link>
+                    <Link to="/">Login</Link>
+                </div>
+
+
+                <Switch>
+                    <Route path="/match/find" component={TextList} />
+                    <Route path="/match/list" component={MatchList} />
+                    <Route path="/" component={Authentication} />
+                </Switch>
+            </div>
+        </Router>
     </CookiesProvider>,
     document.getElementById('root'));
 
